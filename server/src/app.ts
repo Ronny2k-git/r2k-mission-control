964900ecdc2ffc94059a45bbd2ded1d43255e9a6
Ronny2k-git <ronnyedson12@gmail.com>
2026-05-18 14:43:21 -0300

    feat: configure CORS middleware on backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,13 @@
+import cors from "cors";
 import express from "express";
 import { planetsRouter } from "./routes/planets/planets.router";
 
 export const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 app.use(planetsRouter);
